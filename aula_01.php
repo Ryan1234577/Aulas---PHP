7dd70ca5de5ac0f9c09c45ef25a0a8e0be09f755
Ryan Marcos da Silva Costa <ryanmarcosdasilvacosta@gmail.com>
2026-04-08 14:33:25 -0300

    Primeiro upload oficial de Aula PHP

---
+++ aula_01.php
@@ -0,0 +1,16 @@
+<?php
+//echo ("Preparando o ambiente para a aula de PHP 101 <br>");
+//echo nl2br("Minha primeira linha de código em PHP \n");
+//echo nl2br("Minha segunda linha de código em PHP  \n"); // \n quebra de linha
+// \n quebra de linha
+
+$n1 = 10;
+$n2 = 20;
+$resultado = $n1 + $n2;
+//echo "O resultado da soma é: " . $resultado;
+
+if ($resultado > 25) {
+    echo "O resultado é maior que 25";
+} else {
+    echo "O resultado é menor ou igual a 25";
+}
